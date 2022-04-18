4300a5d906ed4215190f93dee060e65be3d99ec2
Arsh Sharma <20BCS7790@cuchd.in>
2022-04-18 20:14:41 +0530

    added new resume and some minor changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ function Resume() {
     return (
         <div id="resumeComp" className=" resumePlacement resumeDiv container d-flex justify-content-center align-items-center" >
             <button className="resumeBtn">
-                <a href="https://drive.google.com/file/d/1XcBTIdMIMwAGSuAg168uqscblYwixn10/view?usp=sharing" target="__blank" ><i className="fa fa-arrow-circle-down"></i> &nbsp; Download Resume</a>
+                <a href="https://drive.google.com/file/d/16t0TvVjt3qifD1DhvLqoZpdQKPp0rz9U/view?usp=sharing" target="__blank" ><i className="fa fa-arrow-circle-down"></i> &nbsp; Download Resume</a>
             </button>
         </div>
     )
